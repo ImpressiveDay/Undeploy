61d9c41cfca235d218f84e3584789c2f6f016e64
ImpressiveDay <155479597+ImpressiveDay@users.noreply.github.com>
2024-04-29 13:02:53 -0400

    cmtfo
    It'll work now, CS does weird stuff w/ directories and users

--- NiceUndeploy.ps1
+++ NiceUndeploy.ps1
@@ -1,13 +1,13 @@
 ### Accept the maintenance token as a string argument ex. "e2226d2b2xxx5ab3xxx0b2cabac801af1xxx1abbccd2xxx3f24b622e6c1f1..."
 $tokenMaint = $args[0]
 
-Set-Location $env:appdata
+Set-Location "C:\"
 New-Item -Name "NiceUndeploy" -ItemType "directory"
-Set-Location .\NiceUndeploy\
+Set-Location "C:\NiceUndeploy\"
 $ProgressPreference = 'SilentlyContinue'
 write "Downloading the removal tool dependency."
 Invoke-WebRequest "https://github.com/ImpressiveDay/Undeploy/raw/main/CsUninstallTool.exe" -OutFile "CsUninstallTool.exe"
 write "Finished download dependency. Awaiting next logic steps."
-.\CsUninstallTool.exe "MAINTENANCE_TOKEN=$tokenMaint" /quiet
+C:\NiceUndeploy\CsUninstallTool.exe "MAINTENANCE_TOKEN=$tokenMaint" /quiet
 write "Received $tokenMaint loud and clear, attempting to start the uninstaller."
 write "The process can take some time, relax."
