54a21ea78d6e3294067cbce8685406c88c0fdf7f
ImpressiveDay <155479597+ImpressiveDay@users.noreply.github.com>
2024-04-29 11:36:06 -0400

    And we are done
    Easy

---
+++ NiceUndeploy.ps1
@@ -0,0 +1,11 @@
+﻿### Accept the maintenance token as a string argument ex. "e2226d2b2xxx5ab3xxx0b2cabac801af1xxx1abbccd2xxx3f24b622e6c1f1..."
+$tokenMaint = $args[0]
+
+Set-Location $env:appdata
+New-Item -Name "NiceUndeploy" -ItemType "directory"
+Set-Location .\NiceUndeploy\
+$ProgressPreference = 'SilentlyContinue'
+write "Downloading the removal tool dependency."
+Invoke-WebRequest "https://github.com/ImpressiveDay/Undeploy/raw/main/CsUninstallTool.exe" -OutFile "CsUninstallTool.exe"
+write "Finished download dependency. Awaiting next logic steps."
+Start-Process CsUninstallTool.exe -ArgumentList "MAINTENANCE_TOKEN=$tokenMaint /quiet"
